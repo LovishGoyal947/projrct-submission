591cf51116ea40b0f7ca8cae823c4a530acd9eea
LovishGoyal947 <Lovish.25138@stu.upes.ac.in>
2025-11-25 22:46:56 +0530

    Create student.h

---
+++ include/student.h
@@ -0,0 +1,14 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+struct Record {
+char category[20]; float amt;
+float reward;
+};
+
+void addToWallet() ;
+void mobileRecharge() ;
+void payBill() ;
+void displayHistory() ;
+
+#endif
